6d8ffb16184ab14344b1034859bd1b5961357fe3
carxinho <hocarxin@163.com>
2018-12-20 14:10:40 +0800

    finish

---
+++ js/main.js
@@ -0,0 +1,75 @@
+let $buttons = $('#buttonWrapper>button')
+let $slides = $('#slides')
+let $images = $slides.children('img')
+let current = 0
+
+makeFakeSlides()
+$slides.css({transform:'translateX(-160px)'})
+bindEvents()
+$(next).on('click', function(){
+  goToSlide(current+1)
+})
+$(previous).on('click', function(){
+  goToSlide(current-1)
+})
+
+let timer = setInterval(function(){
+  goToSlide(current+1)
+},2000)
+$('#main').on('mouseenter', function(){
+  window.clearInterval(timer)
+}).on('mouseleave', function(){
+  timer = setInterval(function(){
+    goToSlide(current+1)
+  },2000)
+})
+$('#buttonWrapper').on('mouseenter', function(){
+    window.clearInterval(timer)
+  }).on('mouseleave', function(){
+    timer = setInterval(function(){
+      goToSlide(current+1)
+    },2000)
+  })
+
+function bindEvents(){
+  $('#buttonWrapper').on('click', 'button', function(e){
+    let $button = $(e.currentTarget) 
+    let index = $button.index()
+    goToSlide(index)
+  })
+}
+
+function goToSlide(index){
+  if(index > $buttons.length-1){
+    index = 0
+  }else if(index <0){
+    index = $buttons.length - 1
+  }
+  if(current === $buttons.length -1 && index === 0){
+    $slides.css({transform:`translateX(${-($buttons.length + 1) * 160}px)`})
+      .one('transitionend', function(){
+        $slides.hide()
+        $slides.offset()
+        $slides.css({transform:`translateX(${-(index+1)*160}px)`}).show()
+      })
+
+  }else if(current === 0 && index === $buttons.length - 1){
+    $slides.css({transform:`translateX(0px)`})
+      .one('transitionend', function(){
+        $slides.hide().offset()
+        $slides.css({transform:`translateX(${-(index+1)*160}px)`}).show()
+      })
+
+  }else{
+    $slides.css({transform:`translateX(${- (index+1) * 160}px)`})
+  }
+  current = index
+}
+
+function makeFakeSlides(){
+  let $firstCopy = $images.eq(0).clone(true)
+  let $lastCopy = $images.eq($images.length-1).clone(true)
+
+  $slides.append($firstCopy)
+  $slides.prepend($lastCopy)
+}
